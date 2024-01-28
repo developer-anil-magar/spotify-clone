56964b8c9287b8cf9addfee03afeee59a9bd1116
developer-anil-magar <anilmagar.developer@gmail.com>
2024-01-29 00:59:41 +0545

    project initialization

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+    <div className="size-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900 text-white">
+      <Header>
+        <h1>Welcome Back</h1>
+      </Header>
+    </div>
+  );
+}
