ab798d172386145737bf704aecf76cd1602d0b39
developer-anil-magar <anilmagar.developer@gmail.com>
2024-01-30 18:03:14 +0545

    adding and fetching songs from supabase

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,12 @@
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/Header';
 import ListItem from '@/components/ListItem';
+import PageContent from './components/PageContent';
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="size-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900 text-white">
       <Header>
@@ -14,7 +19,7 @@ export default function Home() {
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-semibold text-white">Newest songs</h1>
         </div>
-        list of songs coming!!
+        <PageContent songs={songs} />
       </div>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,10 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { twMerge } from 'tailwind-merge';
 import Button from './Button';
 import useAuthModal from '@/hooks/useAuthModal';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useUser } from '@/hooks/useUser';
+import { FaUserAlt } from 'react-icons/fa';
+import toast from 'react-hot-toast';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -16,6 +20,19 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const router = useRouter();
   const authModal = useAuthModal();
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // TODO: reset any playing songs
+    router.refresh();
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('Logged out successfully');
+    }
+  };
   return (
     <div
       className={twMerge(
@@ -96,19 +113,38 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex items-center justify-between gap-x-4">
-          <div>
-            <Button
-              onClick={authModal.onOpen}
-              className="bg-transparent font-medium text-neutral-300"
-            >
-              Sign up
-            </Button>
-          </div>
-          <div>
-            <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
-              Log in
-            </Button>
-          </div>
+          {user ? (
+            <div className="flex items-center gap-x-4">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push('/account')}
+                className=" bg-white "
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-transparent font-medium text-neutral-300"
+                >
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
